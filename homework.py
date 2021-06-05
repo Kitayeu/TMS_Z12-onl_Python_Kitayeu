9446455de7dfca521a8a1f66d98f4056279fa569
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-06-05 18:58:58 +0300

    Added homework - 02
    Worked with string-type variables

---
+++ homework.py
@@ -0,0 +1,18 @@
+# Homework 02
+
+a: str = "Hello world"
+
+# task_2_1
+print(a[2])
+
+# task_2_2
+print(a[-2])
+
+# task_2_3
+print(a[:6:])
+
+# task_2_4
+print(a[:len(a)-2:])
+
+# task_2_5
+print(a[::2])
